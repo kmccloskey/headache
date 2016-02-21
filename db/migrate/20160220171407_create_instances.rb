b356c028291a66541fcb12c1e25813c828d7f09a
Jon Wexler <jwexman@gmail.com>
2016-02-20 20:19:10 -0500

    removed medication, trigger, and symptom columns from instance- instead will be separate models

--- db/migrate/20160220171407_create_instances.rb
+++ db/migrate/20160220171407_create_instances.rb
@@ -2,9 +2,6 @@ class CreateInstances < ActiveRecord::Migration
   def change
     create_table :instances do |t|
     t.string :sense
-    t.string :trigger
-    t.string :symptoms
-    t.string :medication
     t.string :relief
     t.string :affect
     t.string :weather
